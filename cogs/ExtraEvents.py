7eb62b8bfee8665eff63aa61f195913bdf69e679
GreenDevTh <theerapong@rumail.ru.ac.th>
2023-02-05 21:23:00 +0700

    update all system event and story

---
+++ cogs/ExtraEvents.py
@@ -0,0 +1,61 @@
+import discord
+from discord.ext import commands
+from discord.commands import SlashCommandGroup, Option
+from func.events import extra_content, extra_list, extra_event_contents
+
+from scripts.guilds import guild_data
+
+
+guild_id = guild_data()["realistic"]
+
+
+class ExtraEvents(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    extra = SlashCommandGroup(guild_ids=[guild_id], name="extra", description="คำสั่งแสดงผลอีเว้นเพิ่มเติม")
+
+    @extra.command(name="แสดงเนื้อหาอีเว้นเสริม", description="คำสั่งติดตั้งแคตตากอรี่และแชลแนลสำหรับแสดงเนื้อหาอีเว้นเสริม")
+    async def extra_event_installer(
+            self,
+            ctx:discord.Interaction,
+            extra:Option(str, "เลือกอีเว้นที่ต้องการ", choices=extra_list)
+    ):
+        guild = ctx.guild
+        choices = extra_list
+        cate = "EXTRA EVENT"
+        overwrites = {
+            guild.default_role: discord.PermissionOverwrite(
+                view_channel=True,
+                read_messages=True,
+                read_message_history=True,
+                send_messages=False
+            )
+        }
+        try:
+            if discord.utils.get(guild.categories, name=cate):
+                pass
+            else:
+                await guild.create_category(name=cate, overwrites=overwrites)
+        except Exception as e:
+            return await ctx.response.send_message(e, ephemeral=True)
+        else:
+            if extra == choices[int(extra_list.index(extra))]:
+                channel_name = f"📔-{extra}"
+                try:
+                    cates = discord.utils.get(guild.categories, name=cate)
+                    channel = discord.utils.get(guild.channels, name=channel_name)
+                    if channel:
+                        await channel.purge()
+                        await channel.send(embed=extra_event_contents(str(extra)))
+                    else:
+                        channle = await guild.create_text_channel(name=channel_name, category=cates)
+                        await channle.send(embed=extra_event_contents(str(extra)))
+                except Exception as e:
+                    return await ctx.response.send_message(e, ephemeral=True)
+                else:
+                    return await ctx.response.send_message(f"เพิ่มข้อมูลเนื้อหาอีเว้น {extra} ให้กับระบบเรียบร้อย",
+                                                           ephemeral=True)
+def setup(bot):
+    bot.add_cog(ExtraEvents(bot))
--- func/events.py
+++ func/events.py
@@ -1,4 +1,5 @@
 from session.SessionContent import *
+from session.extraContents import *
 
 event_list = [
     "บทที่-1",
@@ -12,16 +13,47 @@ event_list = [
     "บทที่-9",
     "บทที่-10"
 ]
+
+extra_list = [
+    "อีเว้นเสริม-1",
+    "อีเว้นเสริม-2",
+    "อีเว้นเสริม-3",
+    "อีเว้นเสริม-4",
+    "อีเว้นเสริม-5",
+    "อีเว้นเสริม-6",
+    "อีเว้นเสริม-7",
+    "อีเว้นเสริม-8",
+    "อีเว้นเสริม-9",
+    "อีเว้นเสริม-10"
+]
+extra_content = [
+    extra_001()
+]
 content = [
     event_001(),
     event_002(),
     event_003()
 ]
+
+
 def event_contents(x):
     index = event_list.index(x)
     try:
         if content[index]:
             return content[index]
+    except IndexError:
+        print(index)
+        embed = discord.Embed(
+            title="ไม่มีข้อมูล"
+        )
+        return embed
+
+
+def extra_event_contents(x):
+    index = extra_list.index(x)
+    try:
+        if extra_content[index]:
+            return extra_content[index]
     except IndexError:
         print(index)
         embed = discord.Embed(

de1c768ec1b3f8185d20631b819d703712db44f6
GreenDevth <theerapong@rumail.ru.ac.th>
2023-02-15 14:36:11 +0700

    finish the police event

--- Class/The_Police.py
+++ Class/The_Police.py
@@ -15,6 +15,8 @@ from session.SessionContent import police_event
 
 guild_id = guild_data()["realistic"]
 role_list = ["Bandit", "Police"]
+
+
 class ThePoliceCommand(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -24,16 +26,15 @@ class ThePoliceCommand(commands.Cog):
         print(__class__.__name__, " is ready")
         self.bot.add_view(ThePoliceGet())
 
+    police = SlashCommandGroup(guild_ids=[guild_id], name="police",
+                               description="คำสั่งจัดการอีเว้นสกัดกั้นการขนย้ายอาวุธ")
 
-
-    police = SlashCommandGroup(guild_ids=[guild_id], name="police", description="คำสั่งจัดการอีเว้นสกัดกั้นการขนย้ายอาวุธ")
-
-
-    @police.command(name="เปิดคำสั่งใช้งานอีเว้น",description="คำสั่งเปิดการแสดงคำสั่งลงทะเบียนเลือกหน้าที่โดยหัวหน้าหมู่บ้าน")
+    @police.command(name="เปิดคำสั่งใช้งานอีเว้น",
+                    description="คำสั่งเปิดการแสดงคำสั่งลงทะเบียนเลือกหน้าที่โดยหัวหน้าหมู่บ้าน")
     async def prolice_event(
             self,
-            ctx:discord.Interaction,
-            select:Option(str, "เลือกคำสั่งที่ต้องการ", choices=["Enabled", "Disabled"])
+            ctx: discord.Interaction,
+            select: Option(str, "เลือกคำสั่งที่ต้องการ", choices=["Enabled", "Disabled"])
     ):
         choice = ["Enabled", "Disabled"]
         guild = ctx.guild
@@ -41,8 +42,8 @@ class ThePoliceCommand(commands.Cog):
         if select == choice[0]:
             cate_name = "EVENT REGISTER"
             ch_name = "🚧-ด่าน"
-            overwrites={
-                guild.default_role:discord.PermissionOverwrite(
+            overwrites = {
+                guild.default_role: discord.PermissionOverwrite(
                     send_messages=False
                 )
             }
@@ -54,7 +55,7 @@ class ThePoliceCommand(commands.Cog):
                         await channel.purge()
                         await channel.send(embed=police_event(), view=ThePoliceGet())
                         return await ctx.response.send_message("เปิดการใช้งานฟังก์ชั่นการลงทะเบียนเรียบร้อย",
-                                                       ephemeral=True)
+                                                               ephemeral=True)
                 else:
                     category = await guild.create_category(name=cate_name, overwrites=overwrites)
                     channel = await guild.create_text_channel(name=ch_name, category=category)
@@ -63,16 +64,12 @@ class ThePoliceCommand(commands.Cog):
                         if channel:
                             await channel.send(embed=police_event(), view=ThePoliceGet())
                             return await ctx.response.send_message("เปิดการใช้งานฟังก์ชั่นการลงทะเบียนเรียบร้อย",
-                                                           ephemeral=True)
+                                                                   ephemeral=True)
                     except Exception as e:
                         print(e)
 
             except Exception as e:
                 return await ctx.response.send_message(e)
-
-
-
-
 
     @commands.command(name="police")
     async def police_event(self, ctx):
@@ -80,8 +77,6 @@ class ThePoliceCommand(commands.Cog):
         await ctx.message.delete()
         guild = ctx.guild
         room_name = "📝-ผู้ใช้งาน-id-{}".format(member.discriminator)
-
-
 
         channel = get(guild.channels, name=room_name)
         if ctx.channel == channel and member == ctx.author:
@@ -95,46 +90,73 @@ class ThePoliceGet(discord.ui.View):
         super(ThePoliceGet, self).__init__(timeout=None)
 
     @discord.ui.button(label="คลิกเพื่อทำการจับฉลาก", style=discord.ButtonStyle.secondary, custom_id="get_role_police")
-    async def get_role_police(self, button, interaction:discord.Interaction):
+    async def get_role_police(self, button, interaction: discord.Interaction):
         await interaction.response.defer(ephemeral=True, invisible=False)
-        button.disabled=False
+        msg = await interaction.followup.send("⏳ กรุณารอสักครู่ระบบกำลังสุ่มบทบาทให้กับคุณ")
+        button.disabled = False
         member = interaction.user
         police_list = Event_list().police_list()
         city = City().citizen(member.id)[1]
         print(len(police_list))
 
         if len(police_list) == 0:
-            return await interaction.followup.send("ระบบทำการจับฉลากครบแล้ว")
+            return await msg.edit(content="⚠️ ระบบทำการจับฉลากครบแล้ว")
+        if City().citizen(interaction.user.id)[3] == 0:
+            return await msg.edit(content="⚠️ คุณไม่ใช่หัวหน้าทีม ไม่สามารถใช้งานคำสั่งนี้ได้")
+
         if Event_list().check(member.id) == 1:
-            return await interaction.followup.send(f"คุณจับฉลากได้ {Event_list().player(member.id)}")
+            data = Event_list().player(member.id)
 
-        def get_item_list():
+            def detail():
+                embed = discord.Embed(
+                    title=f"{member.display_name} บทบาทที่คุณได้รับ"
+                )
+                embed.set_image(url=img_("event_frist"))
+                if data[3] == "Bandit":
+                    embed.add_field(name="บทบาท", value="กลุ่มผู้ต้องสงสัย")
+                elif data[3] == "Police":
+                    embed.add_field(name="บทบาท", value="ทีมตำรวจประจำด่าน")
+                else:
+                    embed.add_field(name="บทบาท", value="หน่วย SWAT")
+                return embed
 
-            while True:
-                time.sleep(1)
-                try:
-                    match = random.randint(1, 5)
-                    if match in police_list:
-                        Event_list().update_police_list(city, member.id, match)
-                        # print(match)
-                        return match
+            return await msg.edit(content="", embed=detail())
+
+        else:
+
+            def get_item_list():
+
+                while True:
+                    time.sleep(1)
+                    try:
+                        match = random.randint(1, 5)
+                        if match in police_list:
+                            Event_list().update_police_list(city, member.id, match)
+                            # print(match)
+                            return match
+                        else:
+                            pass
+                    except Exception as e:
+                        print(e)
+            try:
+                get_item_list()
+            except Exception as e:
+                return await msg.edit(content=e)
+            else:
+                data = Event_list().player(member.id)
+
+                def detail():
+                    embed = discord.Embed(
+                        title=f"{member.display_name} บทบาทที่คุณได้รับ"
+                    )
+                    embed.set_image(url=img_("event_frist"))
+                    if data[3] == "Bandit":
+                        embed.add_field(name="บทบาท", value="กลุ่มผู้ต้องสงสัย")
+                    elif data[3] == "Police":
+                        embed.add_field(name="บทบาท", value="ทีมตำรวจประจำด่าน")
                     else:
-                        pass
-                except Exception as e:
-                    print(e)
-        #
-        await interaction.followup.send(get_item_list())
+                        embed.add_field(name="บทบาท", value="หน่วย SWAT")
+                    return embed
 
-        #
-        # if City().citizen(interaction.user.id)[3] == 1:
-        #     data = Event_list().event(get_item_list())
-        #     embed= discord.Embed(
-        #         title=data[0],
-        #     )
-        #     embed.add_field(name="เมือง", value=data[1])
-        #     await interaction.response.send_message(embed=embed, ephemeral=True)
-        # else:
-        #     await interaction.response.send_message("คุณไม่ใช่หัวหน้าทีม ไม่สามารถใช้งานคำสั่งนี้ได้", ephemeral=True)
+                return await msg.edit(content="", embed=detail())
 
-
-        
